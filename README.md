bc28fa2b4be11506142069db4176f4eb3b4312e7
Stefan <blockchain.crypto.stefan@gmail.com>
2023-09-04 08:28:03 +0100

    Added Engineer and Intern implementation

--- README.md
+++ README.md
@@ -12,11 +12,13 @@ Run `npm install` or `npm i` to install the dependencies.
 ## Learning Outcomes
 
 - Learnt how to convert Jest to use ES Modules using experimental flags
-- Learnt how to implement recursive logic with functions
+- Implemented recursive logic with functions
 
 ## Resources used
 
 - [Jest Docs: ECMAScript Modules](https://jestjs.io/docs/ecmascript-modules)
+- [Working with folders in Node.js](https://nodejs.dev/en/learn/working-with-folders-in-nodejs/#:~:text=work%20with%20folders.-,Check%20if%20a%20folder%20exists,access%20it%20with%20its%20permissions.)
+- [Check synchronously if file/directory exists in Node.js](https://stackoverflow.com/questions/4482686/check-synchronously-if-file-directory-exists-in-node-js)
 
 ## License
 
--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// const Manager = require("./lib/Manager");
-// const Engineer = require("./lib/Engineer");
-// const Intern = require("./lib/Intern");
 import Manager from "./lib/Manager.js";
 import Engineer from "./lib/Engineer.js";
 import Intern from "./lib/Intern.js";
-// const inquirer = require("inquirer");
-// const path = require("path");
-// const fs = require("fs");
+
 import fs from "fs";
 import inquirer from "inquirer";
 import path from "path";
@@ -20,14 +15,10 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 // const render = require("./src/page-template.js");
 import render from "./src/page-template.js";
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
 const team = []; // array to hold all employee objects
 
+// TODO: Combine and refactor arrays into core questions and per-role questions. Can use spread operator to add into new arrays.
 const initialQuestions = [
-  // Name
-  // Employee ID
-  // Email address
-  // Office number
   {
     type: "input",
     name: "managerName",
@@ -50,6 +41,52 @@ const initialQuestions = [
   },
 ];
 
+const engineerQuestions = [
+  {
+    type: "input",
+    name: "engineerName",
+    message: "What is the name of the engineer?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the employee ID of the engineer?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the email address of the engineer?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is the GitHub username of the engineer?",
+  },
+];
+
+const internQuestions = [
+  {
+    type: "input",
+    name: "internName",
+    message: "What is the name of the intern?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the employee ID of the intern?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the email address of the intern?",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What school does the intern attend?",
+  },
+];
+
 const menuItems = [
   "Add an engineer",
   "Add an intern",
@@ -62,14 +99,14 @@ const menuOptions = [
     name: "menuSelection",
     message: "What would you like to do next?",
     choices: menuItems,
-    // default: "Add an engineer",
   },
 ];
 
-// When a user enters those requirements, the user is presented with a menu with the option to:
-// Add an engineer
-// Add an intern
-// Finish building the team
+// TODO: Update JSDocs comments
+/**
+ * @function showMenu
+ * @returns { function }
+ */
 function showMenu() {
   inquirer.prompt(menuOptions).then((answer) => {
     console.log(answer);
@@ -91,32 +128,53 @@ function showMenu() {
 }
 
 function addEngineer() {
-  // When a user selects the engineer option, the user is prompted to enter the following and then taken back to the menu:
-  // Engineer's Name
-  // ID
-  // Email
-  // GitHub username
-  // implement inquirer questions
-  console.log("addEngineer");
-  showMenu(); // recursively call showMenu until user selects "Finish building the team"
+  inquirer.prompt(engineerQuestions).then((answers) => {
+    console.log(answers);
+
+    // create a new Engineer instance
+    const engineer = new Engineer(
+      answers.engineerName,
+      answers.id,
+      answers.email,
+      answers.github
+    );
+
+    // add the engineer instance to the team array
+    team.push(engineer);
+    console.log(team);
+
+    showMenu(); // recursively call showMenu until user selects "Finish building the team"
+  });
 }
 
 function addIntern() {
-  // When a user selects the intern option, the user is prompted to enter the following and then taken back to the menu:
   // Intern’s name
   // ID
   // Email
   // School
-  // implement inquirer questions
-  console.log("addIntern");
-  showMenu(); // recursively call showMenu until user selects "Finish building the team"
+  inquirer.prompt(internQuestions).then((answers) => {
+    console.log(answers);
+
+    // create a new Intern instance
+    const intern = new Intern(
+      answers.internName,
+      answers.id,
+      answers.email,
+      answers.school
+    );
+
+    // add the intern instance to the team array
+    team.push(intern);
+    console.log(team);
+
+    showMenu(); // recursively call showMenu until user selects "Finish building the team"
+  });
 }
 
 function finishTeam() {
   // implement inquirer questions
   console.log("finishTeam");
-  // write to file
-  // TODO: Call the render function and pass in an array containing all employee objects. Store the result in a variable.
+  // render the HTML and store in a variable
   const html = render(team);
   // TODO: Write the file to the output folder.
   // Create the output directory if it doesn't exist
@@ -124,42 +182,31 @@ function finishTeam() {
     fs.mkdirSync(OUTPUT_DIR);
   }
 
+  // write to file
   fs.writeFile(outputPath, html, (err) => {
     if (err) throw err;
     console.log("File saved!");
   });
-  // When a user decides to finish building their team, they exit the application and the HTML is generated.
 
-  // Call the render function (provided for you) and pass in an array containing all employee objects.
-
-  // The render function will generate and return a block of HTML including templated div elements for each employee.
-
-  // Create an HTML file using the HTML returned from the render function.
-
-  // Write it to a file named team.html in the output folder.
-
-  // You can use the provided variable outputPath to target this location.
   console.log("Exiting application.");
 }
 
 // function to initialize program
 function init() {
-  // When a user starts the application, they're prompted to enter the team manager's:
-  // Name
-  // Employee ID
-  // Email address
-  // Office number
+  // TODO: refactor into addManager() function
   inquirer
     .prompt(initialQuestions)
     .then((answers) => {
       console.log(answers);
-      // create a new Manager object
+
+      // create a new Manager instance
       const manager = new Manager(
         answers.managerName,
         answers.id,
         answers.email,
         answers.officeNumber
       );
+
       // add the manager object to the team array
       team.push(manager);
       console.log(team);
